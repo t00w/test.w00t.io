e16447c87ff83461cda6fa6bdb8c77b19d2fbfae
negue-io <47615006+negue-io@users.noreply.github.com>
2022-10-16 14:07:28 +0200

    temp

--- vendor/js/contact_us.js
+++ vendor/js/contact_us.js
@@ -1,17 +1,11 @@
 //contact us form
 $(".contact_btn").on('click', function () {
     //disable submit button on click
-    // $(".contact_btn").attr("disabled", "disabled");
-    // $(".contact_btn b").text('Sending');
+    $(".contact_btn").attr("disabled", "disabled");
+    $(".contact_btn b").text('Sending');
     $(".contact_btn i").removeClass('d-none');
-
-    //simple validation at client's end
-    var post_data, output;
     var proceed = "true";
-    // var allBlank;
-
-    var str = $('#contact-form-data').serializeArray();
-
+    var name
     $('#contact-form-data input').each(function() {
         if(!$(this).val()){
             // alert('Some fields are empty');
@@ -19,91 +13,49 @@ $(".contact_btn").on('click', function () {
         }
     });
 
-    //everything looks good! proceed...
-    if (proceed === "true") {
-
-        var pathArray = window.location.pathname.split('/');
-        var secondLevelLocation = pathArray[3];
-
-        var accessURL;
-        if(secondLevelLocation){
-            accessURL="../vendor/contact-mailer.php";
-        }else{
-            accessURL="vendor/contact-mailer.php";
-        }
-        //data to be sent to server
-        $.ajax({
-            type: 'POST',
-            // url: 'vendor/contact-mailer.php',
-            url: accessURL,
-            data: str,
-            dataType: 'json',
-            success: function (response) {
-                if (response.type == 'error') {
-                    output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">' + response.text + '</div>';
-                } else {
-                    output = '<div class="alert-success" style="padding:10px 15px; margin-bottom:30px;">' + response.text + '</div>';
-                    //reset values in all input fields
-                    $('.contact-form input').val('');
-                    $('.contact-form textarea').val('');
-                }
-
-                if ($("#result").length) {
-                    // alert("yes");
-                    $("#result").hide().html(output).slideDown();
-                    $(".contact_btn i").addClass('d-none');
-                }else{
-                    if (response.type == 'error') {
-                        Swal.fire({
-                            type: 'error',
-                            icon: 'error',
-                            title: 'Oops...',
-                            html: '<div class="text-danger">'+ response.text +'</div>',
-                        })
-                        $(".contact_btn i").addClass('d-none');
-                    }else{
-                        Swal.fire({
-                            type: 'success',
-                            icon: 'success',
-                            title: 'Success!',
-                            html: '<div class="text-success">'+ response.text +'</div>',
-                        })
-                        $(".contact_btn i").addClass('d-none');
-                    }
-                }
-            },
-            error: function () {
-                alert("Failer");
-            }
-        });
-
-    }
-    else
+    if(proceed == "false")
     {
-        if ($("#result").length) {
-            // alert("yes");
-            output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">Please provide the missing fields.</div>';
-            $("#result").hide().html(output).slideDown();
-            $(".contact_btn i").addClass('d-none');
-        }else{
-            Swal.fire({
-                icon: 'error',
-                type: 'error',
-                title: 'Oops...',
-                html: '<div class="text-danger">Please provide the missing fields.</div>'
-            })
-            $(".contact_btn i").addClass('d-none');
-        }
-
+        output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">Merci de remplir tous les champs.</div>';
+        $("#result").hide().html(output).slideDown();
+        $(".contact_btn i").addClass('d-none');
+        return;
     }
 
+    var frm = {
+        name : $("input#name").val(),
+        email : $("input#email").val(),
+        subject : $("input#subject").val(),
+        message : $("textarea#message").val(),
+    };
+    var json = JSON.stringify(frm);
+    $("#result").hide()
+    $.ajax({
+        type: 'POST',
+        url: "https://sendmailgithubpagesjokrvsep-first.functions.fnc.fr-par.scw.cloud/",
+        data: json,
+        dataType: 'json',
+        success: function (response) 
+        {
+            console.log(response)
+            Swal.fire(
+                'Envoyé',
+                'On vous contacte rapidement !',
+                'success'
+                )
+        },
+        error: function (e) {
+            var output = '<div class="alert-danger" style="padding:10px 15px; margin-bottom:30px;">Merci de remplir tous les champs.</div>';
+            $("#result").hide().html(output).slideDown();
+        }
+    });
 
+    
 });
 
 
 
 //modal window form
-
+/*
 $(".modal_contact_btn").on('click', function () {
     //disable submit button on click
     // $(".modal_contact_btn").attr("disabled", "disabled");
@@ -203,3 +155,4 @@ $(".modal_contact_btn").on('click', function () {
     }
 
 });
+*/
